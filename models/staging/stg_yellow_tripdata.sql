a5787520db662e5cb2ec650950cc6d832117e3a5
pavel.yanachkin@gmail.com <pavel.yanachkin@gmail.com>
2023-02-25 13:17:46 +0000

    task done ver 1.15

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -11,7 +11,7 @@ select
    -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
-    cast(ratecodeid as integer) as ratecodeid,
+    cast(SPLIT(ratecodeID, '.')[OFFSET(0)]  as integer) as ratecodeid,
     cast(pulocationid as integer) as  pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
     
